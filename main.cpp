b65cca7c914937b1dc332e880ace0d885f4b1e1a
YuHsien1127 <101628669+YuHsien1127@users.noreply.github.com>
2022-05-29 21:26:28 +0800

    Add files via upload
    5/29 增加文字與修改.h.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include<iostream>
+#include <ctime>
+#include "Card.h"
+#include "Point.h"
+#include "Player.h"
+#include "QA.h"
 using namespace std;
 
+void Delay(int time)
+{
+	clock_t   now = clock();
+	while (clock() - now < time);
+}
 int main() {
-
+	QA('s');//遊戲規則
+	int n;
+	cin >> n;
+	QA(n,0);
+	string player1, player2;
+	cout << "請輸入名字(Player1): ";
+	cin >> player1;
+	cout << "請輸入名字(Player2): ";
+	cin >> player2;
+	Player P1(player1, 0);
+	Player P2(player2, 0);
+	cout << "\nP1";
+	P1.toString();
+	cout << "P2";
+	P2.toString();
+	Delay(5 * 1000);
+	system("cls");
+	cout << "請閒家在5秒內迴避" << endl;
+	Delay(5 * 1000);
+	system("cls");
 }
