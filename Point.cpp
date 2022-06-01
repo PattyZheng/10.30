937d3a32777d127578f03995d9bd501ea37a7e18
YuHsien1127 <101628669+YuHsien1127@users.noreply.github.com>
2022-06-02 00:53:48 +0800

    Add files via upload
    6/1 .h.cpp編輯

--- Point.cpp
+++ Point.cpp
@@ -1,16 +1,32 @@
 #include "Point.h"
-int Point::CardSum = 0;
-double Point::CardPoint = 0;
 
 Point::Point() {
 
 }
-Point::Point(int c) {
-	CardPoint = c;
+Point::Point(int n) {
+	setCardPoint(n);
+	setCardSum();
+	saveCard(n);
+}
+void Point::setCardPoint(int p) {//算有幾點(11 12 13 = 0.5)
+	if (p > 10) {
+		CardPoint += 0.5;
+	}
+	else
+		CardPoint += p;
 }
 double Point::getCardPoint() {
-	return CardPoint;
+
+}
+void Point::setCardSum() {//幾張牌
+	CardSum++;
 }
 int Point::getCardSum() {
-	return CardSum;
+
+}
+void Point::saveCard(int c) {//儲存拿到的卡
+	playersCard[CardSum] = c;
+}
+int Point::getCard() {
+
 }
--- Point.h
+++ Point.h
@@ -3,13 +3,18 @@
 
 class Point
 {
-private:
-    static double CardPoint;
-    static int CardSum;
 public:
+    int playersCard[5];//存拿到的牌
+    double CardPoint = 0;//點數
+    int CardSum = 0;//牌數
     Point();
     Point(int);
+    void setCardPoint(int);//算有幾點
     double getCardPoint();
+    void setCardSum();//幾張牌
     int getCardSum();
+    void saveCard(int);//儲存拿到的卡
+    int getCard();
+    
 };
 #endif
