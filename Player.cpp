8e0a0173f544671407026eff9030f90c6e362cfb
YuHsien1127 <101628669+YuHsien1127@users.noreply.github.com>
2022-06-04 01:30:59 +0800

    Add files via upload
    6/3編輯

--- Player.cpp
+++ Player.cpp
@@ -4,7 +4,6 @@
 #include <sstream>
 using namespace std;
 
-int Player::PlayerSum = 0;
 Player::Player()
 {
 
@@ -13,6 +12,7 @@ Player::Player(string n, int ps ,Point p)
 {
     name = n;
     ps = PlayerSum;
+    point = p;
 }
 void Player::setName(string n)
 {
@@ -30,12 +30,6 @@ int Player::getPlayerSum()//有
 {
     return PlayerSum;
 }
-void Player::toString()
-{
-    stringstream ss;
-    cout << "name: " << name << " , " << "Point: " << PlayerSum;
-    point.toString();
-}
 void Player::setCard(int c, int n)//有
 {
     playerCard[n - 1] = c;
@@ -45,7 +39,19 @@ int Player::getCard(int i) const
 {
     return playerCard[i - 1];
 }
+string Player::printCard(int c) {
+    stringstream ss;
+    string suits[] = { "黑桃", "愛心", "方塊", "梅花" };
+    string ranks[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9","10", "J", "Q", "K" };
+    string suit = suits[playerCard[c] / 13];//0,1,2,3  22/13 = 1.. 9
+    string rank = ranks[playerCard[c] % 13];
+    ss << suit << ' ' << rank ;
+    return ss.str();
+}
 Point Player::getPoint() const //有
 {
     return point;
+}
+void Player::setPoint(Point p) {
+    point = p;
 }
