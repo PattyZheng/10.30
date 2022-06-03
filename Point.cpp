0acb6c1ff1017b84c79604846dc5cb9d44b0b22e
ZYL450120 <101628435+ZYL450120@users.noreply.github.com>
2022-06-04 01:31:31 +0800

    Add files via upload
    6/3編輯

--- Point.cpp
+++ Point.cpp
@@ -1,39 +1,32 @@
 #include "Point.h"
-#include<iostream>
-using namespace std;
 
-Point::Point()//有
-{
+Point::Point() {
 
 }
-Point::Point(int n)//有
-{
+Point::Point(int n) {
 	setCardPoint(n);
+	setCardSum();
 	saveCard(n);
 }
-void Point::setCardPoint(int p)//算有幾點(11 12 13 = 0.5)
-{
-	if (p > 10)
+void Point::setCardPoint(int p) {//算有幾點(11 12 13 = 0.5)
+	if (p > 10) {
 		CardPoint += 0.5;
+	}
 	else
 		CardPoint += p;
 }
-double Point::getCardPoint()//有
-{
-	return CardPoint;
+double Point::getCardPoint() {
+
 }
-void Point::saveCard(int c)//儲存拿到的卡
-{
+void Point::setCardSum() {//幾張牌
+	CardSum++;
+}
+int Point::getCardSum() {
+
+}
+void Point::saveCard(int c) {//儲存拿到的卡
 	playersCard[CardSum] = c;
 }
-void Point::toString()
-{
-	cout << "CardPoint: " << CardPoint << " , " << "CardSum: " << CardSum << endl;
-}
-void Point::plusCardPoint(int c)
-{
-	if (c % 13 + 1 > 10)
-		CardPoint += 0.5;
-	else
-		CardPoint += c % 13 + 1;
+int Point::getCard() {
+
 }
