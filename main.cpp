8ecfe388573efb4e4e62fe10cd65c5f8e75e0cde
YuHsien1127 <101628669+YuHsien1127@users.noreply.github.com>
2022-06-06 12:20:26 +0800

    Add files via upload
    6/6 修改

--- main.cpp
+++ main.cpp
@@ -13,23 +13,44 @@ void Delay(int time)
 }
 void gameRule()
 {
-		cout << " 十點半遊戲開始 \n" << endl;
-		cout << " 遊戲人數2人，Player1先為莊家，換場莊家也換人。" << endl;
-		cout << " 閒家先開始遊戲，等閒家結束遊戲後，再換莊家開始遊戲。" << endl;
-		cout << " 遊戲規則與計分如下:" << endl;
-		cout << " 若有要加牌請按1，若不要加牌請按0。" << endl;
-		cout << " 若抽到JQK，則為0.5點。" << endl;
-		cout << " 閒家完畢後換莊家要牌。" << endl;
-		cout << " 雙方結束要牌後，開牌並結算該局成績，計分如下 :" << endl;
-		cout << " 每場遊戲的贏家得1分。" << endl;
-		cout << " !!此遊戲不扣分!!" << endl;
-		cout << endl;
-		cout << " 閱讀完畢請按 enter" << endl;
-		cout << "\n\n\n 注 :若該玩家得五張牌該玩家獲勝得1分，結束該局" << endl;
-		_getch();
-		system("cls");
+	cout << " 十點半遊戲開始 \n" << endl;
+	cout << " 遊戲人數2人，Player1先為莊家，換場莊家也換人。" << endl;
+	cout << " 閒家先開始遊戲，等閒家結束遊戲後，再換莊家開始遊戲。" << endl;
+	cout << " 遊戲規則與計分如下:" << endl;
+	cout << " 若有要加牌請按1，若不要加牌請按0。" << endl;
+	cout << " 若抽到JQK，則為0.5點。" << endl;
+	cout << " 閒家完畢後換莊家要牌。" << endl;
+	cout << " 雙方結束要牌後，開牌並結算該局成績，計分如下 :" << endl;
+	cout << " 每場遊戲的贏家得1分。" << endl;
+	cout << " !!此遊戲不扣分!!" << endl;
+	cout << endl;
+	cout << " 閱讀完畢請按 enter" << endl;
+	cout << "\n\n\n 注 :若該玩家得五張牌該玩家獲勝得1分，結束該局" << endl;
+	_getch();
+	system("cls");
 }
-void game(Player &P1, Player &P2)
+bool yasorno() {
+	char nn1;
+	bool n1;
+	cin >> nn1;
+	int n = 0;
+	while (n != -1) {
+		if (nn1 == '0') {
+			n1 = 0;
+			n = -1;
+		}
+		else if (nn1 == '1') {
+			n1 = 1;
+			n = -1;
+		}
+		else {
+			cout << "Your enter is error,plz enter again." << endl;
+			cin >> nn1;
+		}
+	}
+	return n1;
+}
+void game(Player& P1, Player& P2)
 {
 	P1.setPoint(Point(0));
 	P2.setPoint(Point(0));
@@ -53,13 +74,30 @@ void game(Player &P1, Player &P2)
 	cout << " (請記得)" << endl;
 	P2.setCard(c.getCard(cardnum), num);
 	cout << "\n 要加牌按1，不加牌按0" << endl;
-	cin >> n1;
+	//cin >> nn1;
+	//int n = 0;
+	/*while (n != -1) {
+		if (nn1 == '0') {
+			n1 = 0;
+			n = -1;
+		}
+		else if (nn1 == '1') {
+			n1 = 1;
+			n = -1;
+		}
+		else {
+			cout << "your enter is error,plz enter again." << endl;
+			cin >> nn1;
+		}
+	}*/
+	//cin >> n1;
+	n1 = yasorno();
 	system("cls");
 	cardnum++;
 
 	while (n1 && num < 5 && P2.getPoint().getCardPoint() < 10.5)
 	{
-		
+
 		num++;
 		cout << " 閒家(" << P2.getName() << ")第 " << num << " 張牌為 :";
 		c.print(cardnum);
@@ -70,9 +108,10 @@ void game(Player &P1, Player &P2)
 		c.print(cardnum);
 		cout << endl;
 		P2.setCard(c.getCard(cardnum), num);
-		if (P2.getPoint().getCardPoint() < 10.5 && num<5) {
+		if (P2.getPoint().getCardPoint() < 10.5 && num < 5) {
 			cout << "\n 要加牌按1，不加牌按0" << endl;
-			cin >> n1;
+			//cin >> n1;
+			n1 = yasorno();
 		}
 		else
 			Delay(2 * 1000);
@@ -94,9 +133,9 @@ void game(Player &P1, Player &P2)
 		cout << "\n 閒家(" << P2.getName() << ")獲勝，得1分" << endl;
 		P2.plusPlayerSum();
 	}
-	else 
+	else
 		cout << " 換莊家(" << P1.getName() << ")看牌\n 閒家在3秒內迴避";
-		
+
 
 	if (num < 5 && P2.getPoint().getCardPoint() <= 10.5) {
 		num = 1;
@@ -107,12 +146,13 @@ void game(Player &P1, Player &P2)
 		cout << " (請記得)" << endl;
 		P1.setCard(c.getCard(cardnum), num);
 		cout << "\n 要加牌按1，不加牌按0" << endl;
-		cin >> n1;
+		//cin >> n1;
+		n1 = yasorno();
 		system("cls");
 		cardnum++;
 		while (n1 && num < 5 && P1.getPoint().getCardPoint() < 10.5)
 		{
-			
+
 			num++;
 			cout << " 莊家(" << P1.getName() << ")第 " << num << " 張牌為 :";
 			c.print(cardnum);
@@ -125,7 +165,8 @@ void game(Player &P1, Player &P2)
 			P1.setCard(c.getCard(cardnum), num);
 			if (num < 5 && P1.getPoint().getCardPoint() < 10.5) {
 				cout << "\n 要加牌按1，不加牌按0" << endl;
-				cin >> n1;
+				//cin >> n1;
+				n1 = yasorno();
 			}
 			else
 				Delay(2.5 * 1000);
@@ -175,7 +216,7 @@ int main() {
 	while (again)
 	{
 		system("cls");
-		if (gamenum % 2  == 0)
+		if (gamenum % 2 == 0)
 			game(P2, P1);
 		else
 			game(P1, P2);
@@ -186,9 +227,10 @@ int main() {
 		cout << " Player1 " << P1.getName() << " :" << P1.getPlayerSum() << "分" << endl;
 		cout << " Player2 " << P2.getName() << " :" << P2.getPlayerSum() << "分" << endl;
 		cout << "\n 若要繼續玩請按1，若要結束請按0" << endl;
-		cin >> again;
+		//cin >> again;
+		again = yasorno();
 		gamenum++;
-		
+
 	}
 	system("cls");
 	cout << "~~~遊戲結束~~~" << endl;
